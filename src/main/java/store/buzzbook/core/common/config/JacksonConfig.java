e429aa72f7bf559a20f49ccb8e693e8ad3f26e75
Heldennar <krain7127@gmail.com>
2024-06-19 16:53:12 +0900

    fix

--- src/main/java/store/buzzbook/core/common/config/JacksonConfig.java
+++ src/main/java/store/buzzbook/core/common/config/JacksonConfig.java
@@ -11,7 +11,6 @@ public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
-        // 객체 converting 과정에 대해선 여기서 처리하면 됨
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return objectMapper;
--- src/main/java/store/buzzbook/core/entity/product/Review.java
+++ src/main/java/store/buzzbook/core/entity/product/Review.java
@@ -1,32 +1,38 @@
 package store.buzzbook.core.entity.product;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-
 import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
 
 @Entity
 @Getter
 public class Review {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false, length = 255)
-    private String content;
+	@Column(nullable = false, length = 255)
+	private String content;
 
-    private String picturePath;
-    private Long bookId;
+	private String picturePath;
+	private Long bookId;
 
-    @Column(nullable = false)
-    private int reviewScore;
+	@Column(nullable = false)
+	private int reviewScore;
 
-    @ManyToOne
-    @JoinColumn(name = "product_id",nullable = false)
-    private Product product;
+	@ManyToOne
+	@JoinColumn(name = "product_id", nullable = false)
+	private Product product;
 
-    @Column(nullable = false)
-    private Timestamp reviewCreateDate;
+	@Column(nullable = false)
+	private Timestamp reviewCreateDate;
 
 }
