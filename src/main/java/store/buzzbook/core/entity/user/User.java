7dfab33d9f0203bd23c225b4e56d5406ae8ae5b6
Heldennar <krain7127@gmail.com>
2024-06-20 11:15:30 +0900

    fix: 프로필 정리중

--- src/main/java/store/buzzbook/core/entity/user/User.java
+++ src/main/java/store/buzzbook/core/entity/user/User.java
@@ -1,20 +1,17 @@
 package store.buzzbook.core.entity.user;
 
-
 import java.time.ZonedDateTime;
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -60,6 +57,7 @@ public class User {
 	private ZonedDateTime lastLoginDate;
 
 	@NotNull
+	@Enumerated(value = EnumType.STRING)
 	private UserStatus status;
 
 	@NotNull
@@ -74,6 +72,5 @@ public class User {
 	@NotNull
 	@Column(name = "is_admin")
 	private boolean isAdmin;
-
 
 }
