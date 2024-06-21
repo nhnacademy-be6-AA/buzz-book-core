3ef3a3fed53b7133f8f50c3609d5c73c3716e24c
huijun98 <heej1998@gmail.com>
2024-06-21 10:45:23 +0900

    fix : pr 요구사항대로 수정2

---
+++ src/main/java/store/buzzbook/core/entity/product/ProductTag.java
@@ -0,0 +1,36 @@
+package store.buzzbook.core.entity.product;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "product_tag")
+public class ProductTag {
+
+	@Id
+	@Column(name = "product_id")
+	private int productId;
+
+	@Id
+	@Column(name = "tag_id")
+	private int tagId;
+
+	@ManyToOne
+	@MapsId("productId")
+	@JoinColumn(name = "product_id", insertable = false, updatable = false)
+	private Product product;
+
+	@ManyToOne
+	@MapsId("tagId")
+	@JoinColumn(name = "tag_id", insertable = false, updatable = false)
+	private Tag tag;
+}
