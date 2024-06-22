044ed365a115aefde94ac0fa4fc7c15e6127e915
ParkSeolDev <parkseol.dev@gmail.com>
2024-06-22 22:39:05 +0900

    chore: 수정

--- src/main/java/store/buzzbook/core/repository/payment/BillLogRepository.java
+++ src/main/java/store/buzzbook/core/repository/payment/BillLogRepository.java
@@ -9,11 +9,6 @@ import org.springframework.data.repository.query.Param;
 import store.buzzbook.core.entity.payment.BillLog;
 
 public interface BillLogRepository extends JpaRepository<BillLog, Long> {
-	BillLog findByOrder_id(Long id);
-
-	@Query("select b from BillLog b join Order o on b.order.id = o.id where o.user.id = :userId")
-	Page<BillLog> findAllByUserId(@Param("userId") Long userId, Pageable pageable);
-
 	@Query("select b from BillLog b join Order o on b.order.id = o.id where o.user.id = :userId and b.order.id = :orderId")
 	BillLog findByUserIdAndId(@Param("userId") long userId, @Param("orderId") long orderId);
 
