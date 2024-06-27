8faef2179bf78f51678d6e1788fe12841ef134f5
Heldennar <krain7127@gmail.com>
2024-06-27 19:44:38 +0900

    refactor

--- src/main/java/store/buzzbook/core/repository/user/UserRepositoryCustomImpl.java
+++ src/main/java/store/buzzbook/core/repository/user/UserRepositoryCustomImpl.java
@@ -1,14 +1,36 @@
 package store.buzzbook.core.repository.user;
 
+import static store.buzzbook.core.entity.user.QGrade.*;
+import static store.buzzbook.core.entity.user.QGradeLog.*;
+import static store.buzzbook.core.entity.user.QUser.*;
+
+import java.util.Optional;
+
 import org.springframework.transaction.annotation.Transactional;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
+import store.buzzbook.core.entity.user.Grade;
 
 @RequiredArgsConstructor
 @Transactional
 public class UserRepositoryCustomImpl implements UserRepositoryCustom {
 	private final JPAQueryFactory jpaQueryFactory;
 
+	@Override
+	public Optional<Grade> findGradeByLoginId(String loginId) {
+
+		Grade targetGrade = jpaQueryFactory.select(grade)
+			.from(user)
+			.innerJoin(gradeLog)
+			.on(user.id.eq(gradeLog.user.id))
+			.innerJoin(grade)
+			.on(grade.id.eq(gradeLog.grade.id))
+			.where(user.loginId.eq(loginId))
+			.orderBy(gradeLog.changeAt.asc())
+			.fetchFirst();
+
+		return Optional.ofNullable(targetGrade);
+	}
 }
