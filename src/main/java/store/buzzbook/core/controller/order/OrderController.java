a4c7fda888749bd9dd87b49979b225992ee3802e
ParkSeolDev <parkseol.dev@gmail.com>
2024-06-30 02:45:16 +0900

    chore: toss 폴더 삭제

--- src/main/java/store/buzzbook/core/controller/order/OrderController.java
+++ src/main/java/store/buzzbook/core/controller/order/OrderController.java
@@ -91,99 +91,99 @@ public class OrderController {
 	}
 
 	@Operation(summary = "주문 상태 이름으로 조회", description = "주문 상태 조회")
-	@GetMapping("status/{name}")
+	@GetMapping("/status/{name}")
 	public ResponseEntity<ReadOrderStatusResponse> getOrderStatusByName(@PathVariable String name) {
 		return ResponseEntity.ok(orderService.readOrderStatusByName(name));
 	}
 
 	@Operation(summary = "주문 상태 아이디로 조회", description = "주문 상태 조회")
-	@GetMapping("status/{id}")
+	@GetMapping("/status/{id}")
 	public ResponseEntity<ReadOrderStatusResponse> getOrderStatusById(@PathVariable int id) {
 		return ResponseEntity.ok(orderService.readOrderStatusById(id));
 	}
 
 	@Operation(summary = "주문 상태 모두 조회", description = "주문 상태 모두 조회")
-	@GetMapping("status")
+	@GetMapping("/status")
 	public ResponseEntity<List<ReadOrderStatusResponse>> getAllOrderStatus() {
 		return ResponseEntity.ok(orderService.readAllOrderStatus());
 	}
 
 	@Operation(summary = "주문 상태 등록", description = "주문 상태 등록")
-	@PostMapping("status")
+	@PostMapping("/status")
 	public ResponseEntity<ReadOrderStatusResponse> createOrderStatus(@RequestBody CreateOrderStatusRequest request) {
 		return ResponseEntity.ok(orderService.createOrderStatus(request));
 	}
 
 	@Operation(summary = "주문 상태 수정", description = "주문 상태 수정")
-	@PutMapping("status")
+	@PutMapping("/status")
 	public ResponseEntity<ReadOrderStatusResponse> updateOrderStatus(@RequestBody UpdateOrderStatusRequest request) {
 		return ResponseEntity.ok(orderService.updateOrderStatus(request));
 	}
 
 	@Operation(summary = "주문 상태 삭제", description = "주문 상태 삭제")
-	@DeleteMapping("status/{id}")
+	@DeleteMapping("/status/{id}")
 	public ResponseEntity<String> deleteOrderStatus(@PathVariable int id) {
 		orderService.deleteOrderStatus(id);
 		return ResponseEntity.ok(SUCCESS);
 	}
 
 	@Operation(summary = "운임비 정책 조회", description = "운임비 정책 조회")
-	@GetMapping("delivery-policy/{id}")
+	@GetMapping("/delivery-policy/{id}")
 	public ResponseEntity<ReadDeliveryPolicyResponse> getDeliveryPolicy(@PathVariable int id) {
 		return ResponseEntity.ok(orderService.readDeliveryPolicyById(id));
 	}
 
 	@Operation(summary = "운임비 정책 모두 조회", description = "운임비 정책 모두 조회")
-	@GetMapping("delivery-policy")
+	@GetMapping("/delivery-policy")
 	public ResponseEntity<List<ReadDeliveryPolicyResponse>> getAllDeliveryPolicy() {
 		return ResponseEntity.ok(orderService.readAllDeliveryPolicy());
 	}
 
 	@Operation(summary = "운임비 정책 등록", description = "운임비 정책 등록")
-	@PostMapping("delivery-policy")
+	@PostMapping("/delivery-policy")
 	public ResponseEntity<ReadDeliveryPolicyResponse> createDeliveryPolicy(@RequestBody CreateDeliveryPolicyRequest request) {
 		return ResponseEntity.ok(orderService.createDeliveryPolicy(request));
 	}
 
 	@Operation(summary = "운임비 정책 수정", description = "운임비 정책 수정")
-	@PutMapping("delivery-policy")
+	@PutMapping("/delivery-policy")
 	public ResponseEntity<ReadDeliveryPolicyResponse> updateDeliveryPolicy(@RequestBody UpdateDeliveryPolicyRequest request) {
 		return ResponseEntity.ok(orderService.updateDeliveryPolicy(request));
 	}
 
 	@Operation(summary = "운임비 정책 삭제", description = "운임비 정책 삭제")
-	@DeleteMapping("delivery-policy/{id}")
+	@DeleteMapping("/delivery-policy/{id}")
 	public ResponseEntity<String> deleteDeliveryPolicy(@PathVariable int id) {
 		orderService.deleteDeliveryPolicy(id);
 		return ResponseEntity.ok(SUCCESS);
 	}
 
 	@Operation(summary = "포장 조회", description = "포장 조회")
-	@GetMapping("wrapping/{id}")
+	@GetMapping("/wrapping/{id}")
 	public ResponseEntity<ReadWrappingResponse> getWrapping(@PathVariable int id) {
 		return ResponseEntity.ok(orderService.readWrappingById(id));
 	}
 
 	@Operation(summary = "포장 모두 조회", description = "포장 모두 조회")
-	@GetMapping("wrapping")
+	@GetMapping("/wrapping")
 	public ResponseEntity<List<ReadWrappingResponse>> getAllWrappings() {
 		return ResponseEntity.ok(orderService.readAllWrapping());
 	}
 
 	@Operation(summary = "포장 등록", description = "포장 등록")
-	@PostMapping("wrapping")
+	@PostMapping("/wrapping")
 	public ResponseEntity<ReadWrappingResponse> createWrapping(@RequestBody CreateWrappingRequest request) {
 		return ResponseEntity.ok(orderService.createWrapping(request));
 	}
 
 	@Operation(summary = "포장 수정", description = "포장 수정")
-	@PutMapping("wrapping")
+	@PutMapping("/wrapping")
 	public ResponseEntity<ReadWrappingResponse> updateWrapping(@RequestBody UpdateWrappingRequest request) {
 		return ResponseEntity.ok(orderService.updateWrapping(request));
 	}
 
 	@Operation(summary = "포장 삭제", description = "포장 삭제")
-	@DeleteMapping("wrapping/{id}")
+	@DeleteMapping("/wrapping/{id}")
 	public ResponseEntity<String> deleteWrapping(@PathVariable int id) {
 		orderService.deleteWrapping(id);
 		return ResponseEntity.ok(SUCCESS);
