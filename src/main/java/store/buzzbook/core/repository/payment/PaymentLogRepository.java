1f1643bd290ff622b01e6e18bb91659114fb26b5
ParkSeolDev <parkseol.dev@gmail.com>
2024-06-30 14:59:38 +0900

    feat: 결제 수단 이력 조회 구현

--- src/main/java/store/buzzbook/core/repository/payment/PaymentLogRepository.java
+++ src/main/java/store/buzzbook/core/repository/payment/PaymentLogRepository.java
@@ -1,8 +1,12 @@
 package store.buzzbook.core.repository.payment;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import store.buzzbook.core.entity.payment.PaymentLog;
 
 public interface PaymentLogRepository extends JpaRepository<PaymentLog, Integer> {
+	List<PaymentLog> findByOrder_OrderStrAndOrder_User_LoginId(String orderStr, String loginId);
+	List<PaymentLog> findByOrder_OrderStr(String orderStr);
 }
