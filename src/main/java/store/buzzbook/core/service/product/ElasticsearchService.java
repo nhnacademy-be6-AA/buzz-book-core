26b9b2355827edd83c6025e3ccc7dcc3b8ee6858
huijun98 <heej1998@gmail.com>
2024-07-01 14:18:54 +0900

    fix : 카트 컨트롤러 주석처리, 엘라스틱 서치 List형태로 반환하도록 변경

--- src/main/java/store/buzzbook/core/service/product/ElasticsearchService.java
+++ src/main/java/store/buzzbook/core/service/product/ElasticsearchService.java
@@ -3,7 +3,14 @@ package store.buzzbook.core.service.product;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.util.Base64;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import store.buzzbook.core.common.client.ElasticSearchClient;
+import store.buzzbook.core.document.product.ProductDocument;
 
 @Service
 public class ElasticsearchService {
@@ -20,8 +27,13 @@ public class ElasticsearchService {
 		this.elasticSearchClient = elasticSearchClient;
 	}
 
-	public String searchProducts(String query) {
+	public List<ProductDocument> searchProducts(String query) throws JsonProcessingException {
 		String token = Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
-		return elasticSearchClient.searchProducts(query, "Basic " + token);
+		String response = elasticSearchClient.searchProducts(query,"Basic "+token);
+
+		//Json응답 -> ProductDocument 리스트로 변환
+		ObjectMapper objectMapper = new ObjectMapper();
+		List<ProductDocument> products = objectMapper.readValue(response, new TypeReference<List<ProductDocument>>() {});
+		return products;
 	}
 }
