172ae74a1c1f62c77dbe77de39f762fdb68d70b3
park <qmzo553@gmail.com>
2024-07-03 13:50:29 +0900

    :bug: fix: conflict merge

--- src/main/java/store/buzzbook/core/common/config/RabbitmqConfig.java
+++ src/main/java/store/buzzbook/core/common/config/RabbitmqConfig.java
@@ -14,13 +14,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class RabbitmqConfig {
 
-	public static final String REQUEST_EXCHANGE_NAME = "aa.coupon.welcome.exchange";
-	public static final String REQUEST_QUEUE_NAME = "aa.coupon.welcome.queue";
-	public static final String REQUEST_ROUTING_KEY = "aa.coupon.welcome.key";
+	public static final String REQUEST_EXCHANGE_NAME = "aa.coupon.exchange";
+	public static final String REQUEST_QUEUE_NAME = "aa.coupon.queue";
+	public static final String REQUEST_ROUTING_KEY = "aa.coupon.key";
 
 	public static final String DLX_EXCHANGE_NAME = "aa.coupon.dlx.exchange";
 	public static final String DLQ_QUEUE_NAME = "aa.coupon.dlx.queue";
