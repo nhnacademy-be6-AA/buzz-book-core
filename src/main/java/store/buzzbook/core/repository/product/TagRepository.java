05f7f00aa30015f27dee7fa00e1c24f4e3619af8
huijun98 <heej1998@gmail.com>
2024-07-04 21:28:44 +0900

    fix : elastic Search 실행에 문제되는 요인 - Tagname 삭제

--- src/main/java/store/buzzbook/core/repository/product/TagRepository.java
+++ src/main/java/store/buzzbook/core/repository/product/TagRepository.java
@@ -1,6 +1,5 @@
 package store.buzzbook.core.repository.product;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +8,6 @@ import store.buzzbook.core.entity.product.Tag;
 
 public interface TagRepository extends JpaRepository<Tag, Integer> {
 
-	List<Tag> findByProductId(int productId);
+
 	Optional<Tag> findByName(String name);
 }
