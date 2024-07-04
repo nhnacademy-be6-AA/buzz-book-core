7655931a7b96ec2d26a52880bd58d75ea7ae32e7
huijun98 <heej1998@gmail.com>
2024-07-04 13:31:25 +0900

    feat : Product jwt 관련 Aop추가

---
+++ src/main/java/store/buzzbook/core/common/aop/ProductJwtAop.java
@@ -0,0 +1,51 @@
+package store.buzzbook.core.common.aop;
+
+import java.util.Map;
+import java.util.Objects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import store.buzzbook.core.common.exception.auth.AuthorizeFailException;
+import store.buzzbook.core.service.auth.AuthService;
+
+@Component
+@RequiredArgsConstructor
+@Aspect
+public class ProductJwtAop {
+	private final AuthService authService;
+	private final HttpServletRequest request;
+
+	//Product AdminPage jwt 토큰
+	@Before("@annotation(store.buzzbook.core.common.annotation.JwtAdminValidate)")
+	public void authenticate(JoinPoint joinPoint) throws Throwable {
+		String authorizationHeader = request.getHeader("Authorization");
+
+		if (Objects.isNull(authorizationHeader)) {
+			throw new AuthorizeFailException("JWT 토큰이 없습니다.");
+		}
+
+		Map<String, Object> claims = authService.getInfoMapFromJwt(request);
+
+		Long userId = (Long) claims.get(AuthService.USER_ID);
+		String loginId = (String) claims.get(AuthService.LOGIN_ID);
+		String role = (String) claims.get(AuthService.ROLE);
+
+		if (Objects.isNull(userId) || Objects.isNull(loginId) || Objects.isNull(role)) {
+			throw new AuthorizeFailException("사용자 정보가 null 입니다.");
+		}
+
+		if (!"admin".equals(role)) {
+			throw new AuthorizeFailException("접근 권한이 없습니다.");
+		}
+
+		request.setAttribute(AuthService.USER_ID, userId);
+		request.setAttribute(AuthService.LOGIN_ID, loginId);
+		request.setAttribute(AuthService.ROLE, role);
+	}
+
+}
--- src/main/java/store/buzzbook/core/controller/product/ProductController.java
+++ src/main/java/store/buzzbook/core/controller/product/ProductController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
