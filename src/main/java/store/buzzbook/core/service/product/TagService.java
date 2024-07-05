7675006e19de87ddf8aba474de80dad745746d4c
huijun98 <heej1998@gmail.com>
2024-07-05 15:00:25 +0900

    fix : Tag pageable 서비스 메서드 및 트랜젝션 애노테이션 추가

--- src/main/java/store/buzzbook/core/service/product/TagService.java
+++ src/main/java/store/buzzbook/core/service/product/TagService.java
@@ -1,8 +1,13 @@
 package store.buzzbook.core.service.product;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import store.buzzbook.core.common.exception.product.DataNotFoundException;
@@ -16,23 +21,35 @@ public class TagService {
 
 	private final TagRepository tagRepository;
 
+	@Transactional
 	public TagResponse saveTag(String tagName) {
 		Tag tag = new Tag(tagName);
 			tagRepository.save(tag);
 		return TagResponse.convertToTagResponse(tag);
 	}
 
+	@Transactional(readOnly = true)
 	public List<TagResponse> getAllTags() {
 		List<Tag> tagList = tagRepository.findAll();
 		return tagList.stream().map(TagResponse::convertToTagResponse).toList();
 	}
 
+	@Transactional
+	public Page<TagResponse> getAllTags(Pageable pageable) {
+		return tagRepository.findAll(pageable).map(TagResponse::convertToTagResponse);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<TagResponse> getTagsByName(String name, Pageable pageable) {
+		return tagRepository.findByNameContainingIgnoreCase(name, pageable).map(TagResponse::convertToTagResponse);
+	}
+	@Transactional(readOnly = true)
 	public TagResponse getTagById(int tagId) {
 		Tag tag = tagRepository.findById(tagId)
 			.orElseThrow(() -> new DataNotFoundException("tag", tagId));
 		return TagResponse.convertToTagResponse(tag);
 	}
-
+	@Transactional(readOnly = true)
 	public TagResponse getTagByName(String tagName) {
 		Tag tag = tagRepository.findByName(tagName).orElse(null);
 		if (tag == null) {
@@ -41,6 +58,7 @@ public class TagService {
 		return TagResponse.convertToTagResponse(tag);
 	}
 
+	@Transactional
 	public void deleteTag(int tagId) {
 		Tag tag = tagRepository.findById(tagId)
 			.orElseThrow(() -> new DataNotFoundException("tag", tagId));
