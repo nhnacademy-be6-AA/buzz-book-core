a370d9230fde54413bc9b52e414b75e9ef78140d
huijun98 <heej1998@gmail.com>
2024-07-05 14:23:24 +0900

    fix : Tag requestMapping 추가

--- src/main/java/store/buzzbook/core/controller/product/TagController.java
+++ src/main/java/store/buzzbook/core/controller/product/TagController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,7 @@ import store.buzzbook.core.service.product.TagService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/tags")
 @Tag(name = "태그 관리", description = "태그 C_UD")
 public class TagController {
 
