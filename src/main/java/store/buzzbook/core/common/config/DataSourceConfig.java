59099511d2d306cd65acb4352cba2f57dcd55582
ParkSeolDev <parkseol.dev@gmail.com>
2024-07-05 17:11:31 +0900

    feat: 키매니저 설정

--- src/main/java/store/buzzbook/core/common/config/DataSourceConfig.java
+++ src/main/java/store/buzzbook/core/common/config/DataSourceConfig.java
@@ -10,6 +10,7 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 import javax.sql.DataSource;
 
