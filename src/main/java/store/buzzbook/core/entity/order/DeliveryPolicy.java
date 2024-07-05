6b2eb30c7177697865ae9c9db252c7dff13275c6
ParkSeolDev <parkseol.dev@gmail.com>
2024-07-05 10:43:27 +0900

    feat: order, payment entity validation

--- src/main/java/store/buzzbook/core/entity/order/DeliveryPolicy.java
+++ src/main/java/store/buzzbook/core/entity/order/DeliveryPolicy.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,6 +22,7 @@ public class DeliveryPolicy {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Size(min = 1, max = 20)
 	private String name;
 	@NotNull
 	private int standardPrice;
