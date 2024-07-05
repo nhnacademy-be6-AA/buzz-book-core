4d8f963a3e4763ae1c6cf668955dd03995259340
ParkSeolDev <parkseol.dev@gmail.com>
2024-07-05 10:42:04 +0900

    feat: order, payment entity validation

--- src/main/java/store/buzzbook/core/entity/order/OrderDetail.java
+++ src/main/java/store/buzzbook/core/entity/order/OrderDetail.java
@@ -14,6 +14,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -65,5 +66,6 @@ public class OrderDetail {
 	@NotNull
 	private LocalDateTime createAt;
 
+	@Size(min = 1, max = 14)
 	private String couponCode;
 }
