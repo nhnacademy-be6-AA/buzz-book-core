c9a2190ec718c54595a43e310342c4f76aedc64d
huijun98 <heej1998@gmail.com>
2024-07-05 16:21:57 +0900

    fix : TagController Restful 하게 이름 변경

--- src/main/java/store/buzzbook/core/controller/product/TagController.java
+++ src/main/java/store/buzzbook/core/controller/product/TagController.java
@@ -41,8 +41,8 @@ public class TagController {
 		return ResponseEntity.ok(tagService.saveTag(tagName));
 	}
 
-	@GetMapping
-	@Operation(summary = "태그 조회", description = "주어진 (String) 조회<br>태그를 id 순서로 조회<br>주어진 id(int)에 해당하는 태그 조회")
+	@GetMapping("/all")
+	@Operation(summary = "태그 조회(List)", description = "주어진 (String) 조회<br>태그를 id 순서로 조회<br>주어진 id(int)에 해당하는 태그 조회")
 	@ApiResponse(responseCode = "200", description = "조회 성공시 List<TagResponse> 반환<br>태그 이름(String) 값 존재하면 이름으로 조회한 태그 반환")
 
 	public ResponseEntity<List<TagResponse>> getAllTags(
@@ -54,7 +54,10 @@ public class TagController {
 	}
 
 	//임시적으로 Page 버전으로 만듦
-	@GetMapping("/all")
+	@GetMapping
+	@Operation(summary = "태그 조회(Page)", description = "주어진 (String) 조회<br>태그를 id 순서로 조회<br>주어진 id(int)에 해당하는 태그 조회")
+	@ApiResponse(responseCode = "200", description = "조회 성공시 List<TagResponse> 반환<br>태그 이름(String) 값 존재하면 이름으로 조회한 태그 반환")
+
 	public ResponseEntity<Page<TagResponse>> getAllTags(
 		@RequestParam(required = false, defaultValue = "0") @Parameter(description = "페이지 번호") Integer pageNo,
 		@RequestParam(required = false, defaultValue = "10") @Parameter(description = "한 페이지에 보여질 아이템 수") Integer pageSize,
@@ -81,11 +84,13 @@ public class TagController {
 		return ResponseEntity.ok(tagService.getTagById(id));
 	}
 
-	@DeleteMapping
+	@DeleteMapping("/{id}")
 	@Operation(summary = "태그 삭제", description = "주어진 id(int)에 해당하는 태그 삭제.")
 	@ApiResponse(responseCode = "204", description = "삭제 성공시")
 	public ResponseEntity<Void> deleteTag(@RequestParam int tagId) {
 		tagService.deleteTag(tagId);
 		return ResponseEntity.noContent().build();
 	}
+
+
 }
