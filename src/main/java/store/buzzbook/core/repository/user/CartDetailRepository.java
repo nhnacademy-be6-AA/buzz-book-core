6c74f90c6c7e723b5d7019945104cdb4cb716144
Heldennar <37499129+Heldennar@users.noreply.github.com>
2024-07-06 21:05:19 +0900

    feat : 중복 상품 카트에 담을 경우 분기점

--- src/main/java/store/buzzbook/core/repository/user/CartDetailRepository.java
+++ src/main/java/store/buzzbook/core/repository/user/CartDetailRepository.java
@@ -1,5 +1,7 @@
 package store.buzzbook.core.repository.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import store.buzzbook.core.entity.cart.Cart;
@@ -7,4 +9,6 @@ import store.buzzbook.core.entity.cart.CartDetail;
 
 public interface CartDetailRepository extends JpaRepository<CartDetail, Long> {
 	void deleteAllByCart(Cart cart);
+
+	Optional<CartDetail> findByProductIdAndCartId(int productId, long cartId);
 }
