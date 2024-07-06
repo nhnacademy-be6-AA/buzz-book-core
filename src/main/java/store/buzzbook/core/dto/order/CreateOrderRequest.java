39875d419542937b5d2d69072a4d7ad3fb1a49ee
ParkSeolDev <parkseol.dev@gmail.com>
2024-07-07 02:08:17 +0900

    chore: 수정

--- src/main/java/store/buzzbook/core/dto/order/CreateOrderRequest.java
+++ src/main/java/store/buzzbook/core/dto/order/CreateOrderRequest.java
@@ -23,7 +23,6 @@ public class CreateOrderRequest {
 	private String loginId;
 	private List<CreateOrderDetailRequest> details;
 	private String contactNumber;
-	private String email;
 	private Integer orderStatusId;
 	private String sender;
 	private String receiverContactNumber;
