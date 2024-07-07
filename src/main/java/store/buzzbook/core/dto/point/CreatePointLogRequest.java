8fa5b7237ff2fe310668649578995c84aea6c77b
park <qmzo553@gmail.com>
2024-07-07 15:04:41 +0900

    :seedling: feat: create point log controller

--- src/main/java/store/buzzbook/core/dto/point/CreatePointLogRequest.java
+++ src/main/java/store/buzzbook/core/dto/point/CreatePointLogRequest.java
@@ -4,8 +4,9 @@ import jakarta.validation.constraints.NotBlank;
 
 public record CreatePointLogRequest(
 
-	@NotBlank
+	@NotBlank(message = "적립 사유는 빈 칸일 수 없습니다.")
 	String inquiry,
+
 	int deltaPoint
 ) {
 }
