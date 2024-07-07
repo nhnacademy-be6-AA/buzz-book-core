bcc898d3487a6bd569de3be142681cf8bd61561a
Heldennar <37499129+Heldennar@users.noreply.github.com>
2024-07-07 20:58:43 +0900

    docs : 주소 서비스, 회원 서비스, 등급서비스 추가

--- src/main/java/store/buzzbook/core/service/user/GradeService.java
+++ src/main/java/store/buzzbook/core/service/user/GradeService.java
@@ -2,6 +2,18 @@ package store.buzzbook.core.service.user;
 
 import store.buzzbook.core.entity.user.Grade;
 
+/**
+ * 회원 등급 관리를 위한 서비스입니다.
+ * @author Heldenarr
+ */
 public interface GradeService {
+	/**
+	 * 등급 저장입니다. 이름이 중복될 경우 저장되지 않습니다.
+	 *
+	 * @author Heldenarr
+	 * @param grade 저장할 등급 객체입니다.
+	 *
+	 */
 	void save(Grade grade);
+
 }
