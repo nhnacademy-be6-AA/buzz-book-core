be096937f91b075c64b8dcde1f1bfe85cb0edba6
s49e <dydqja1ghk@hanmail.net>
2024-07-07 11:40:09 +0900

    save : 카테고리 임시저장

--- src/main/java/store/buzzbook/core/controller/product/CategoryController.java
+++ src/main/java/store/buzzbook/core/controller/product/CategoryController.java
@@ -2,12 +2,19 @@ package store.buzzbook.core.controller.product;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import store.buzzbook.core.dto.product.CategoryRequest;
 import store.buzzbook.core.dto.product.CategoryResponse;
 import store.buzzbook.core.service.product.CategoryService;
 
@@ -19,8 +26,30 @@ public class CategoryController {
 
 	private final CategoryService categoryService;
 
-	@GetMapping
+	@GetMapping("/all")
 	public List<CategoryResponse> getAllCategories() {
 		return categoryService.getAllCategoryResponses();
+	}
+
+	@GetMapping
+	public Page<CategoryResponse> getAllCategories(
+		@RequestParam(required = false) Integer pageNo,
+		@RequestParam(required = false) Integer pageSize) {
+		return categoryService.getPageableCategoryResponses(pageNo, pageSize);
+	}
+
+	@PostMapping
+	public CategoryResponse createCategory(@RequestBody CategoryRequest categoryRequest) {
+		return categoryService.createCategory(categoryRequest);
+	}
+
+	@PostMapping("/{id}")
+	public CategoryResponse updateCategory(@PathVariable("id") int id, @RequestBody CategoryRequest categoryRequest) {
+		return categoryService.updateCategory(id, categoryRequest);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteCategory(@PathVariable("id") int id) {
+		categoryService.deleteCategory(id);
 	}
 }
--- src/main/java/store/buzzbook/core/service/product/CategoryService.java
+++ src/main/java/store/buzzbook/core/service/product/CategoryService.java
@@ -1,12 +1,18 @@
 package store.buzzbook.core.service.product;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import store.buzzbook.core.common.exception.product.DataNotFoundException;
+import store.buzzbook.core.dto.product.CategoryRequest;
 import store.buzzbook.core.dto.product.CategoryResponse;
+import store.buzzbook.core.entity.product.Category;
 import store.buzzbook.core.repository.product.CategoryRepository;
 
 @Service
@@ -15,9 +21,44 @@ public class CategoryService {
 
 	private final CategoryRepository categoryRepository;
 
+	public CategoryResponse createCategory(CategoryRequest categoryRequest) {
+		Category parentCategory = null;
+		if (categoryRequest.getParentCategory() != null) {
+			int parentId = categoryRequest.getParentCategory();
+			parentCategory = categoryRepository.findById(parentId).orElseThrow(() -> new DataNotFoundException("parent category", parentId));
+		}
+		Category category = new Category(categoryRequest.getName(), parentCategory);
+		return CategoryResponse.convertToCategoryResponse(categoryRepository.save(category));
+	}
+
 	public List<CategoryResponse> getAllCategoryResponses() {
 		return categoryRepository.findAll().stream()
 			.map(CategoryResponse::convertToCategoryResponse)
-			.collect(Collectors.toList());
+			.toList();
+	}
+
+	public Page<CategoryResponse> getPageableCategoryResponses(int page, int size) {
+		Pageable pageable = PageRequest.of(page, size);
+		return categoryRepository.findAll(pageable).map(CategoryResponse::convertToCategoryResponse);
+	}
+
+	public CategoryResponse updateCategory(int categoryId, CategoryRequest categoryRequest) {
+		if(categoryRepository.existsById(categoryId)) {
+			throw new DataNotFoundException("category", categoryId);
+		}
+		Category parentCategory = null;
+		if (categoryRequest.getParentCategory() != null) {
+			int parentId = categoryRequest.getParentCategory();
+			parentCategory = categoryRepository.findById(parentId).orElseThrow(() -> new DataNotFoundException("parent category", parentId));
+		}
+		Category category = new Category(categoryId, categoryRequest.getName(), parentCategory);
+		return CategoryResponse.convertToCategoryResponse(categoryRepository.save(category));
+	}
+
+	public void deleteCategory(int categoryId) {
+		if(categoryRepository.existsById(categoryId)) {
+			throw new DataNotFoundException("category", categoryId);
+		}
+		categoryRepository.deleteById(categoryId);
 	}
 }
