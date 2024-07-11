3029e972211574e1b6085154a258e76af0f47ed4
s49e <dydqja1ghk@hanmail.net>
2024-07-11 23:07:09 +0900

    save : 리뷰중간저장

--- src/main/java/store/buzzbook/core/client/image/CloudImageClient.java
+++ src/main/java/store/buzzbook/core/client/image/CloudImageClient.java
@@ -2,6 +2,7 @@ package store.buzzbook.core.client.image;
 
 import java.util.List;
 
+import org.json.simple.JSONObject;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
+import netscape.javascript.JSObject;
+
 @FeignClient(name = "cloudImageClient", url = "https://api-image.nhncloudservice.com")
 public interface CloudImageClient {
 
 	@PostMapping(value = "/image/v2.0/appkeys/${nhncloud.image.appkey}/images", consumes = "multipart/form-data")
-	ResponseEntity<String> uploadImages(
+	ResponseEntity<JSONObject> uploadImages(
 		@RequestHeader("Authorization") String authorization,
 		@RequestPart("params") String params,
 		@RequestPart("files") List<MultipartFile> files
--- src/main/java/store/buzzbook/core/service/image/ImageService.java
+++ src/main/java/store/buzzbook/core/service/image/ImageService.java
@@ -1,24 +1,33 @@
 package store.buzzbook.core.service.image;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import store.buzzbook.core.client.image.CloudImageClient;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class ImageService {
 
+	private static final String REVIEW_FOLDER_PATH = "/aa-image/review";
+
 	private final CloudImageClient cloudImageClient;
+	private final ObjectMapper objectMapper;
 
 	@Value("${nhncloud.image.appkey}")
 	private String appKey;
@@ -38,10 +47,41 @@ public class ImageService {
 
 		try {
 			String paramsJson = new ObjectMapper().writeValueAsString(paramsMap);
-			ResponseEntity<String> response = cloudImageClient.uploadImages(secretKey, paramsJson, files);
-			return response.getBody();
+			ResponseEntity<JSONObject> response = cloudImageClient.uploadImages(secretKey, paramsJson, files);
+			return Objects.requireNonNull(response.getBody()).toJSONString();
 		} catch (Exception e) {
 			throw new RuntimeException("Failed to upload images", e);
 		}
+
+	}
+
+	public List<String> multiImageUpload(List<MultipartFile> files) {
+
+		Map<String, Object> paramsMap = Map.of(
+			"basepath", REVIEW_FOLDER_PATH,
+			"overwrite", false,
+			"autorename", true
+		);
+		try {
+			String paramsJson = new ObjectMapper().writeValueAsString(paramsMap);
+			ResponseEntity<JSONObject> response = cloudImageClient.uploadImages(secretKey, paramsJson, files);
+			JsonNode successesNode = objectMapper.readTree(Objects.requireNonNull(response.getBody()).toString())
+				.get("successes");
+			List<String> imageUrls = new ArrayList<>();
+			if (successesNode != null && successesNode.isArray()) {
+				for (JsonNode success : successesNode) {
+					String imageUrl = success.get("url").asText();
+					imageUrls.add(imageUrl);
+				}
+			}
+			return imageUrls;
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	public String reviewImageUpload(MultipartFile file) {
+		return multiImageUpload(List.of(file)).getFirst();
 	}
 }
