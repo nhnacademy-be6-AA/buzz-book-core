98a2f69fdb1870f3a528ae1f58aeafd704e06b8f
ParkSeolDev <parkseol.dev@gmail.com>
2024-07-13 12:25:59 +0900

    chore: 예외처리

--- src/main/java/store/buzzbook/core/common/exception/handler/OrderExceptionHandler.java
+++ src/main/java/store/buzzbook/core/common/exception/handler/OrderExceptionHandler.java
@@ -1,4 +1,4 @@
-package store.buzzbook.core.common.exception;
+package store.buzzbook.core.common.exception.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +10,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import lombok.extern.slf4j.Slf4j;
 import store.buzzbook.core.common.exception.order.AddressNotFoundException;
 import store.buzzbook.core.common.exception.order.DeliveryPolicyNotFoundException;
+import store.buzzbook.core.common.exception.order.ExpiredToRefundException;
+import store.buzzbook.core.common.exception.order.NotPaidException;
+import store.buzzbook.core.common.exception.order.OrderDetailNotFoundException;
 import store.buzzbook.core.common.exception.order.OrderNotFoundException;
 import store.buzzbook.core.common.exception.order.OrderStatusNotFoundException;
 import store.buzzbook.core.common.exception.order.ProductNotFoundException;
@@ -20,7 +23,8 @@ import store.buzzbook.core.common.exception.order.WrappingNotFoundException;
 public class OrderExceptionHandler extends ResponseEntityExceptionHandler {
 	@ExceptionHandler(value = {DeliveryPolicyNotFoundException.class, OrderStatusNotFoundException.class,
 		ProductNotFoundException.class, WrappingNotFoundException.class, AddressNotFoundException.class,
-		OrderNotFoundException.class})
+		OrderNotFoundException.class, AddressNotFoundException.class, ExpiredToRefundException.class,
+		NotPaidException.class, OrderDetailNotFoundException.class})
 	public ResponseEntity<String> handleOrderNotFound(Exception ex, WebRequest request) {
 		log.debug("Handling order exception : {}", ex.getMessage());
 		return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
