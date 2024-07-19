54dd21a8582f1348bcf01c5d695ed1d5ed072eb1
huijun98 <heej1998@gmail.com>
2024-07-19 09:35:46 +0900

    chore : 불필요한 import 삭제

--- src/test/java/store/buzzbook/core/controller/product/BookControllerTest.java
+++ src/test/java/store/buzzbook/core/controller/product/BookControllerTest.java
@@ -19,7 +19,6 @@ import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
--- src/test/java/store/buzzbook/core/controller/product/BookSearchControllerTest.java
+++ src/test/java/store/buzzbook/core/controller/product/BookSearchControllerTest.java
@@ -8,14 +8,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import store.buzzbook.core.dto.product.BookApiRequest;
 import store.buzzbook.core.service.product.BookSearchService;
-
-import java.util.List;
-
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
