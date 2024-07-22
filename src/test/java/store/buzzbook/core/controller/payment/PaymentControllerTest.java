96c08cdfe6d50bd6ac4d1410f666429ac03a19e2
ParkSeolDev <parkseol.dev@gmail.com>
2024-07-22 15:29:08 +0900

    feat: 캐시 매니저 등록

--- src/test/java/store/buzzbook/core/controller/payment/PaymentControllerTest.java
+++ src/test/java/store/buzzbook/core/controller/payment/PaymentControllerTest.java
@@ -4,7 +4,9 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static store.buzzbook.core.common.listener.OrderStatusListener.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,15 +20,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import jakarta.servlet.http.HttpServletRequest;
+import store.buzzbook.core.dto.payment.CreateBillLogRequest;
+import store.buzzbook.core.dto.payment.CreateCancelBillLogRequest;
 import store.buzzbook.core.dto.payment.ReadBillLogRequest;
 import store.buzzbook.core.dto.payment.ReadBillLogResponse;
 import store.buzzbook.core.dto.payment.ReadBillLogWithoutOrderResponse;
 import store.buzzbook.core.dto.payment.ReadBillLogsRequest;
+import store.buzzbook.core.dto.payment.ReadPaymentKeyRequest;
 import store.buzzbook.core.dto.payment.ReadPaymentResponse;
+import store.buzzbook.core.dto.user.GradeInfoResponse;
 import store.buzzbook.core.dto.user.UserInfo;
 import store.buzzbook.core.entity.payment.BillStatus;
 import store.buzzbook.core.service.auth.AuthService;
@@ -52,16 +60,25 @@ class PaymentControllerTest {
 	@MockBean
 	private UserService userService;
 
+	private UserInfo testUserInfo;
 	private List<ReadBillLogWithoutOrderResponse> readBillLogWithoutOrderResponses = new ArrayList<>();
 	private ReadBillLogWithoutOrderResponse readBillLogWithoutOrderResponse;
 	private ReadBillLogRequest readBillLogRequest;
+	private ReadPaymentKeyRequest readPaymentKeyRequest;
+	private ReadPaymentKeyRequest readPaymentKeyRequest2;
 
 	@BeforeEach
 	public void setUp() {
+		testUserInfo = new UserInfo(504L, "parkseol", "01011111111", "parkseol",
+			"parkseol.dev@gmail.com", LocalDate.parse("2024-06-28"), GradeInfoResponse.builder().benefit(0.03)
+			.name("PLATINUM").standard(300000).build(), true, 5000);
+
 		readBillLogWithoutOrderResponse = new ReadBillLogWithoutOrderResponse(1L, "간편결제", 1000, LocalDateTime.now(),
 			BillStatus.DONE, "tviva20240721175933qQnE4", null);
 		readBillLogWithoutOrderResponses.add(readBillLogWithoutOrderResponse);
 		readBillLogRequest = new ReadBillLogRequest("MC4zODU1NzE0MTc1NzQy");
+		readPaymentKeyRequest = new ReadPaymentKeyRequest("MC4xMDE2OTQyNzMzOTg5", "parkseol.dev@gmail.com");
+		readPaymentKeyRequest2 = new ReadPaymentKeyRequest("MC4xMDE2OTQyNzMzOTg5", null);
 	}
 
 	@Test
@@ -164,4 +181,135 @@ class PaymentControllerTest {
 
 		verify(paymentService).createCancelBillLog(any(JSONObject.class));
 	}
+
+	@Test
+	@DisplayName("포인트, 쿠폰 결제 내역 추가 - 로그인")
+	void createBillLogForDifferentPayment_LoggedIn() throws Exception {
+		CreateBillLogRequest createBillLogRequest = new CreateBillLogRequest();
+		ReadBillLogResponse readBillLogResponse = new ReadBillLogResponse();
+
+		when(paymentService.createBillLogWithDifferentPayment(any(CreateBillLogRequest.class), any(HttpServletRequest.class)))
+			.thenReturn(readBillLogResponse);
+
+		mockMvc.perform(post("/api/payments/bill-log/different-payment")
+				.content(objectMapper.writeValueAsString(createBillLogRequest))
+				.contentType(MediaType.APPLICATION_JSON)
+				.with(request -> {
+					request.setAttribute(AuthService.LOGIN_ID, "testLoginId");
+					return request;
+				}))
+			.andExpect(status().isOk())
+			.andExpect(content().json(objectMapper.writeValueAsString(readBillLogResponse)));
+
+		verify(paymentService).createBillLogWithDifferentPayment(any(CreateBillLogRequest.class), any(HttpServletRequest.class));
+	}
+
+	@Test
+	@DisplayName("포인트, 쿠폰 취소 내역 추가 - 로그인")
+	void createCancelBillLogForDifferentPayment_LoggedIn() throws Exception {
+		CreateCancelBillLogRequest createCancelBillLogRequest = new CreateCancelBillLogRequest();
+
+		mockMvc.perform(post("/api/payments/bill-log/different-payment/cancel")
+				.content(objectMapper.writeValueAsString(createCancelBillLogRequest))
+				.contentType(MediaType.APPLICATION_JSON)
+				.with(request -> {
+					request.setAttribute(AuthService.LOGIN_ID, "testLoginId");
+					return request;
+				}))
+			.andExpect(status().isOk())
+			.andExpect(content().string("Canceled"));
+
+		verify(paymentService).createCancelBillLogWithDifferentPayment(any(CreateCancelBillLogRequest.class), any(HttpServletRequest.class));
+	}
+
+	@Test
+	@DisplayName("포인트, 쿠폰 환불 내역 추가 - 로그인")
+	void createRefundBillLogForDifferentPayment_LoggedIn() throws Exception {
+		CreateCancelBillLogRequest createCancelBillLogRequest = new CreateCancelBillLogRequest();
+
+		mockMvc.perform(post("/api/payments/bill-log/different-payment/refund")
+				.content(objectMapper.writeValueAsString(createCancelBillLogRequest))
+				.contentType(MediaType.APPLICATION_JSON)
+				.with(request -> {
+					request.setAttribute(AuthService.LOGIN_ID, "testLoginId");
+					return request;
+				}))
+			.andExpect(status().isOk())
+			.andExpect(content().string("Canceled"));
+
+		verify(paymentService).createRefundBillLogWithDifferentPayment(any(CreateCancelBillLogRequest.class), any(HttpServletRequest.class));
+	}
+
+	// @Test
+	// @DisplayName("결제키 조회 - 비회원")
+	// void getPaymentKey_NotLoggedIn() throws Exception {
+	// 	when(paymentService.getPaymentKeyWithoutLogin(readPaymentKeyRequest.getOrderId(), readPaymentKeyRequest.getOrderEmail()))
+	// 		.thenReturn(anyString());
+	//
+	// 	mockMvc.perform(post("/api/payments/payment-key")
+	// 			.content(objectMapper.writeValueAsString(readPaymentKeyRequest))
+	// 			.contentType(MediaType.APPLICATION_JSON))
+	// 		.andExpect(status().isOk())
+	// 		.andExpect(content().string(anyString()));
+	//
+	// 	verify(paymentService).getPaymentKeyWithoutLogin(anyString(), anyString());
+	// }
+
+	// @Test
+	// @DisplayName("결제키 조회 - 회원")
+	// void getPaymentKey_LoggedIn() throws Exception {
+	// 	when(userService.getUserInfoByLoginId(anyString())).thenReturn(testUserInfo);
+	// 	when(paymentService.getPaymentKey(readPaymentKeyRequest2.getOrderId(), testUserInfo.id())).thenReturn(anyString());
+	//
+	// 	mockMvc.perform(post("/api/payments/payment-key")
+	// 			.content(objectMapper.writeValueAsString(readPaymentKeyRequest))
+	// 			.contentType(MediaType.APPLICATION_JSON)
+	// 			.requestAttr(AuthService.LOGIN_ID, "parkseol"))
+	// 		.andExpect(status().isOk())
+	// 		.andExpect(content().string(anyString()));
+	//
+	// 	verify(paymentService).getPaymentKey(anyString(), anyLong());
+	// }
+
+	// @Test
+	// @DisplayName("주문상세 아이디로 결제키 조회 - 비회원")
+	// void getPaymentKeyWithOrderDetailId_NotLoggedIn() throws Exception {
+	// 	ReadPaymentKeyWithOrderDetailRequest readPaymentKeyWithOrderDetailRequest = new ReadPaymentKeyWithOrderDetailRequest();
+	// 	readPaymentKeyWithOrderDetailRequest.setOrderDetailId(123L);
+	// 	readPaymentKeyWithOrderDetailRequest.setOrderEmail("test@example.com");
+	//
+	// 	when(orderService.readOrderStr(anyLong())).thenReturn("orderStr");
+	// 	when(paymentService.getPaymentKeyWithoutLogin(anyString(), anyString()))
+	// 		.thenReturn("responseKey");
+	//
+	// 	mockMvc.perform(post("/api/payments/detail/payment-key")
+	// 			.content(objectMapper.writeValueAsString(readPaymentKeyWithOrderDetailRequest))
+	// 			.contentType(MediaType.APPLICATION_JSON))
+	// 		.andExpect(status().isOk())
+	// 		.andExpect(content().string("responseKey"));
+	//
+	// 	verify(paymentService).getPaymentKeyWithoutLogin(eq("orderStr"), eq("test@example.com"));
+	// }
+
+	// @Test
+	// @DisplayName("주문상세 아이디로 결제키 조회 - 회원")
+	// void getPaymentKeyWithOrderDetailId_LoggedIn() throws Exception {
+	// 	ReadPaymentKeyWithOrderDetailRequest readPaymentKeyWithOrderDetailRequest = new ReadPaymentKeyWithOrderDetailRequest();
+	// 	readPaymentKeyWithOrderDetailRequest.setOrderDetailId(123L);
+	// 	UserInfo userInfo = new UserInfo();
+	// 	userInfo.setId(1L);
+	//
+	// 	when(userService.getUserInfoByLoginId(anyString())).thenReturn(userInfo);
+	// 	when(orderService.readOrderStr(anyLong())).thenReturn("orderStr");
+	// 	when(paymentService.getPaymentKey(anyString(), anyLong())).thenReturn("responseKey");
+	//
+	// 	mockMvc.perform(post("/api/payments/detail/payment-key")
+	// 			.content(objectMapper.writeValueAsString(readPaymentKeyWithOrderDetailRequest))
+	// 			.contentType(MediaType.APPLICATION_JSON)
+	// 			.requestAttr(AuthService.LOGIN_ID, "testLoginId"))
+	// 		.andExpect(status().isOk())
+	// 		.andExpect(content().string("responseKey"));
+	//
+	// 	verify(paymentService).getPaymentKey(eq("orderStr"), eq(1L));
+	// }
 }
