4bba22cddebf9b3c4d3216c79ebc058dbb7b1a8a
s49e <dydqja1ghk@hanmail.net>
2024-07-25 13:37:19 +0900

    feat : 리뷰기능 업그레이드 - 리뷰 안쓴 주문 조회

--- src/main/java/store/buzzbook/core/repository/order/OrderDetailRepository.java
+++ src/main/java/store/buzzbook/core/repository/order/OrderDetailRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import store.buzzbook.core.entity.order.Order;
 import store.buzzbook.core.entity.order.OrderDetail;
 
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
