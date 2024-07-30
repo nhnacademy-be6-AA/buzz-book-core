67d5a0e6517033f180fb3f61779a0225346cde29
huijun98 <heej1998@gmail.com>
2024-07-30 15:41:25 +0900

    fix : 불필요한 import 제거

--- src/main/java/store/buzzbook/core/elastic/repository/ProductDocumentRepository.java
+++ src/main/java/store/buzzbook/core/elastic/repository/ProductDocumentRepository.java
@@ -1,6 +1,5 @@
 package store.buzzbook.core.elastic.repository;
 
-import java.util.List;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import store.buzzbook.core.elastic.document.BookDocument;
